39fc4ae3b6ccd536124767b993d711666a1c8248
nadyakit <nadezda.sysol2@mail.ru>
2021-04-02 12:32:40 +0300

    Update 1center_jaba_s1.js

--- js/1center_jaba_s1.js
+++ js/1center_jaba_s1.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
-$('.center_jaba_s1').click(
-  function(){
-    $('.center_jaba_s1').toggleClass('.center_jaba_s2').removeClass('.center_jaba_s1');
-  },
-  function(){
-    $('.center_jaba_s2').css('visibility', 'visible')
-      $('.center_jaba_s1').css('visibility', 'hidden')
-  },
-);
-}
+  $('.center_jaba_s1').click(
+    function(){
+      $('.center_jaba_s1').toggleClass('.center_jaba_s2').removeClass('.center_jaba_s1');
+    },
+    function(){
+        $('.center_jaba_s2').css('visibility', 'visible')
+        $('.center_jaba_s1').css('visibility', 'hidden')
+      },
+    );
+  }
