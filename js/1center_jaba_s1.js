3c2c3b1b8b7ef4fabb29cc242a9753c483247bae
nadyakit <nadezda.sysol2@mail.ru>
2021-04-02 13:28:35 +0300

    Update 1center_jaba_s1.js

--- js/1center_jaba_s1.js
+++ js/1center_jaba_s1.js
@@ -20,24 +20,87 @@ $(document).ready(function(){
 
   })
 
-  $(document).ready(function(){
-    $('.center_jaba2_s1').click(
+$(document).ready(function(){
+  $('.center_jaba2_s1').click(
+    function(){
+      $('.center_jaba2_s1').toggleClass('.center_jaba2_s2').removeClass('.center_jaba2_s1');
+    },
+    function(){
+        $('.center_jaba2_s2').css('visibility', 'visible')
+        $('.center_jaba2_s1').css('visibility', 'hidden')
+      },
+    );
+    $('.center_jaba2_s2').click(
+        function(){
+            $('.center_jaba2_s2').toggleClass('.center_jaba2_s1').removeClass('.center_jaba2_s2');
+        },
+        function(){
+            $('.center_jaba2_s1').css('visibility', 'visible')
+            $('.center_jaba2_s2').css('visibility', 'hidden')
+        },
+    );
+
+  })
+$(document).ready(function(){
+  $('.center_jaba3_s1').click(
+    function(){
+      $('.center_jaba3_s1').toggleClass('.center_jaba3_s2').removeClass('.center_jaba3_s1');
+    },
+    function(){
+        $('.center_jaba3_s2').css('visibility', 'visible')
+        $('.center_jaba3_s1').css('visibility', 'hidden')
+      },
+    );
+    $('.center_jaba3_s2').click(
+        function(){
+            $('.center_jaba3_s2').toggleClass('.center_jaba3_s1').removeClass('.center_jaba3_s2');
+        },
+        function(){
+            $('.center_jaba3_s1').css('visibility', 'visible')
+            $('.center_jaba3_s2').css('visibility', 'hidden')
+        },
+    );
+
+  })
+
+$(document).ready(function(){
+  $('.center_jaba4_s1').click(
+    function(){
+      $('.center_jaba4_s1').toggleClass('.center_jaba4_s2').removeClass('.center_jaba4_s1');
+    },
+    function(){
+        $('.center_jaba4_s2').css('visibility', 'visible')
+        $('.center_jaba4_s1').css('visibility', 'hidden')
+      },
+    );
+    $('.center_jaba4_s2').click(
+        function(){
+            $('.center_jaba4_s2').toggleClass('.center_jaba4_s1').removeClass('.center_jaba4_s2');
+        },
+        function(){
+            $('.center_jaba4_s1').css('visibility', 'visible')
+            $('.center_jaba4_s2').css('visibility', 'hidden')
+        },
+    );
+
+})
+$(document).ready(function(){
+    $('.center_jaba5_s1').click(
       function(){
-        $('.center_jaba2_s1').toggleClass('.center_jaba2_s2').removeClass('.center_jaba2_s1');
+        $('.center_jaba5_s1').toggleClass('.center_jaba5_s2').removeClass('.center_jaba5_s1');
       },
       function(){
-          $('.center_jaba2_s2').css('visibility', 'visible')
-          $('.center_jaba2_s1').css('visibility', 'hidden')
-        },
-      );
-      $('.center_jaba2_s2').click(
-          function(){
-              $('.center_jaba2_s2').toggleClass('.center_jaba2_s1').removeClass('.center_jaba2_s2');
-          },
-          function(){
-              $('.center_jaba2_s1').css('visibility', 'visible')
-              $('.center_jaba2_s2').css('visibility', 'hidden')
-          },
-      );
-
-    })
+        $('.center_jaba5_s2').css('visibility', 'visible')
+        $('.center_jaba5_s1').css('visibility', 'hidden')
+      },
+    );
+    $('.center_jaba5_s2').click(
+      function(){
+          $('.center_jaba5_s2').toggleClass('.center_jaba5_s1').removeClass('.center_jaba5_s2');
+      },
+      function(){
+          $('.center_jaba5_s1').css('visibility', 'visible')
+          $('.center_jaba5_s2').css('visibility', 'hidden')
+      },
+    );
+})
